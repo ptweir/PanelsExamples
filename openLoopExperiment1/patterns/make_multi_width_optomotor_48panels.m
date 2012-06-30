b6b4419bb4170df7def2c5397206910eb29d14f6
Peter Weir <weir@caltech.edu>
2012-06-30 14:43:13 -0300

    Added 4 px (15 deg) spatial wavelength

--- openLoopExperiment1/patterns/make_multi_width_optomotor_48panels.m
+++ openLoopExperiment1/patterns/make_multi_width_optomotor_48panels.m
@@ -1,28 +1,30 @@
 % make_multi_width_optomotor_48panels
 
 pattern.x_num = 96; 	% There are 96 pixel around the display (12x8) 
-pattern.y_num = 8; 		% frames of Y, number of different spatial frequencies
+pattern.y_num = 9; 		% frames of Y, number of different spatial frequencies
 pattern.num_panels = 48; 	% This is the number of unique Panel IDs required.
 pattern.gs_val = 1; 	% This pattern will use 2 intensity levels
 pattern.row_compression = 1;
 
 Pats = zeros(4, 96, pattern.x_num, pattern.y_num);
+% 4 pixel period; spat. freq. 15 degs/cycle
+Pats(:, :, 1, 1) = repmat([ones(4,2) zeros(4,2)], 1, 24); 
 % 6 pixel period; spat. freq. 22.5 degs/cycle
-Pats(:, :, 1, 1) = repmat([ones(4,3) zeros(4,3)], 1, 16); 
+Pats(:, :, 1, 2) = repmat([ones(4,3) zeros(4,3)], 1, 16); 
 % 8 pixel period; spat. freq. 30 degs/cycle
-Pats(:, :, 1, 2) = repmat([ones(4,4) zeros(4,4)], 1, 12); 
+Pats(:, :, 1, 3) = repmat([ones(4,4) zeros(4,4)], 1, 12); 
 % 12 pixel period; spat. freq. 45 degs/cycle
-Pats(:, :, 1, 3) = repmat([ones(4,6) zeros(4,6)], 1, 8); 
+Pats(:, :, 1, 4) = repmat([ones(4,6) zeros(4,6)], 1, 8); 
 % 16 pixel period; spat. freq. 60 degs/cycle
-Pats(:, :, 1, 4) = repmat([ones(4,8) zeros(4,8)], 1, 6); 
+Pats(:, :, 1, 5) = repmat([ones(4,8) zeros(4,8)], 1, 6); 
 % 24 pixel period; spat. freq. 90 degs/cycle
-Pats(:, :, 1, 5) = repmat([ones(4,12) zeros(4,12)], 1, 4);
+Pats(:, :, 1, 6) = repmat([ones(4,12) zeros(4,12)], 1, 4);
 % 32 pixel period; spat. freq. 120 degs/cycle
-Pats(:, :, 1, 6) = repmat([ones(4,16) zeros(4,16)], 1, 3);
+Pats(:, :, 1, 7) = repmat([ones(4,16) zeros(4,16)], 1, 3);
 % 48 pixel period; spat. freq. 180 degs/cycle
-Pats(:, :, 1, 7) = repmat([ones(4,24) zeros(4,24)], 1, 2);
+Pats(:, :, 1, 8) = repmat([ones(4,24) zeros(4,24)], 1, 2);
 % 96 pixel period; spat. freq. 360 degs/cycle
-Pats(:, :, 1, 8) = [ones(4,48) zeros(4,48)];
+Pats(:, :, 1, 9) = [ones(4,48) zeros(4,48)];
 
 for j = 2:96
     for i = 1:pattern.y_num
